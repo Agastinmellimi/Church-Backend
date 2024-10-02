d352812b457dd3b44d1fa0ac7e256de714cb2b9b
Agastinmellimi <agastinmellimi2001@gmail.com>
2024-10-02 11:32:57 +0530

    updated

--- query.sql
+++ query.sql
@@ -2,7 +2,7 @@
 -- INSERT INTO children(name, gender, image) VALUES("NITHIPUDI LALXMITEJA", "MALE", "https://res.cloudinary.com/dkrpgt9kd/image/upload/v1723623058/zly79h3fadpj6tmdzpnr.png"), ("BHOLA DAESHIT KARTHIK", "MALE", "https://res.cloudinary.com/dkrpgt9kd/image/upload/v1723623059/j0wc60oqk6uniyxl9d6l.png");
 
 -- CREATE TABLE attendance(child_id INTEGER, date DATE, status BOOLEAN DEFAULT false, FOREIGN KEY (child_id) REFERENCES children(id) ON DELETE CASCADE);
--- UPDATE children SET id=13 WHERE id =16;
+-- UPDATE children SET name="DAKSHITH KARTHIK BHOLA" WHERE id =13;
 -- SELECT DISTINCT date FROM attendance;
 -- ALTER TABLE children
 -- ADD image STRING;
