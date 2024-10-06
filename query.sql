a58496e77b66ef467a481b316a93e3de7f8c7e85
Agastinmellimi <agastinmellimi2001@gmail.com>
2024-10-06 10:19:47 +0530

    attendance updated 6th oct

--- query.sql
+++ query.sql
@@ -1,14 +1,14 @@
 -- CREATE TABLE children(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name STRING, gender STRING);
--- INSERT INTO children(name, gender, image) VALUES("NITHIPUDI LALXMITEJA", "MALE", "https://res.cloudinary.com/dkrpgt9kd/image/upload/v1723623058/zly79h3fadpj6tmdzpnr.png"), ("BHOLA DAESHIT KARTHIK", "MALE", "https://res.cloudinary.com/dkrpgt9kd/image/upload/v1723623059/j0wc60oqk6uniyxl9d6l.png");
+-- INSERT INTO children(name, gender) VALUES("PRINSHI SHALINI PALLI", "FEMALE");
 
 -- CREATE TABLE attendance(child_id INTEGER, date DATE, status BOOLEAN DEFAULT false, FOREIGN KEY (child_id) REFERENCES children(id) ON DELETE CASCADE);
 -- UPDATE children SET name="DAKSHITH KARTHIK BHOLA" WHERE id =13;
 -- SELECT DISTINCT date FROM attendance;
 -- ALTER TABLE children
 -- ADD image STRING;
--- UPDATE children
--- SET image = "https://res.cloudinary.com/dkrpgt9kd/image/upload/v1723623059/j0wc60oqk6uniyxl9d6l.png"
--- WHERE id=14;
+UPDATE children
+SET id=14
+WHERE id=17;
 
 -- DELETE FROM attendance WHERE child_id=13;
 
